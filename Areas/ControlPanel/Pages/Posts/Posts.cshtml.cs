b3282aba16aca284ff1995c1d4304c8f4f0e39e6
Серёжа <gorbulevsv@yandex.ru>
2020-11-21 23:21:42 +0300

    добавлена область для панели управления

---
+++ Areas/ControlPanel/Pages/Posts/Posts.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorHramBabynino.Areas.ControlPanel.Pages.Posts
+{
+    public class PostsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
