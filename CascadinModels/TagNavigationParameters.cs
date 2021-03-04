7cb415e4c2025f0ab3bb10eeafbab92f76d71625
Серёжа <gorbulevsv@yandex.ru>
2021-03-04 16:17:28 +0300

    35

---
+++ CascadinModels/TagNavigationParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace razorHramBabynino.CascadinModels
+{
+    public class TagNavigationParameters
+    {
+        public string Title { get; set; }
+    }
+}
