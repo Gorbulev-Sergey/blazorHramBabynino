efd37cb3126b2b888acdbcef6758306249fcd35e
gorbulevsv <gorbulevsv@yandex.ru>
2022-03-27 18:01:37 +0300

    2

--- wwwroot/my_js/fullScreen.js
+++ wwwroot/my_js/fullScreen.js
@@ -11,3 +11,15 @@ var enableFullScreen = function () {
 var disableFullScreen = function () {
     document.exitFullscreen();
 }
+
+
+////////////////////////////////////////// Для андройда
+var goToFullscreen = function (albumId) {
+    //console.log("в полный экран " + albumId);
+    window.JSInterface.goToFullscreen(albumId);    
+}
+
+var returnBack = function () {
+    //console.log("Назад");
+    window.JSInterface.returnBack();    
+}
