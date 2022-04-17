91b23bbf01178111cda31ecdf47872f78f0b927b
gorbulevsv <gorbulevsv@yandex.ru>
2022-04-17 21:41:59 +0300

    2

--- wwwroot/my_js/changeBootstrap.js
+++ wwwroot/my_js/changeBootstrap.js
@@ -7,3 +7,19 @@
         localStorage.setItem('bootstrap', '/bootstrap/css/bootstrap.min.css');
     }
 }
+
+// Функция для андройд
+let setLightTheme = function () {
+    if (document.querySelector('#bootstrap').href != "/bootstrap/css/bootstrap.min.css") {
+        document.querySelector('#bootstrap').href = "/bootstrap/css/bootstrap.min.css";
+        localStorage.setItem('bootstrap', '/bootstrap/css/bootstrap.min.css');
+    }
+
+}
+let setDarkTheme = function () {
+    if (document.querySelector('#bootstrap').href != "/bootstrap/css/invert/bootstrap.min.css") {
+        document.querySelector('#bootstrap').href = "/bootstrap/css/invert/bootstrap.min.css";
+        localStorage.setItem('bootstrap', '/bootstrap/css/invert/bootstrap.min.css');
+    }
+
+}
