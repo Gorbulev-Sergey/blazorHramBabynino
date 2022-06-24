2bbc8f471c18f7405459b5dd69ef43b0022afdc9
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-06-24 13:40:44 +0300

    2

--- Program.cs
+++ Program.cs
@@ -15,7 +15,8 @@ using blazorHramBabynino.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<user, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders()
